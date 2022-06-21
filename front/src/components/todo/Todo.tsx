583e1e050c4dc09cabbc231b4b7b2b6bc95a7f7e
maltepoo <jaengpin@gmail.com>
2022-06-21 16:24:11 +0900

    feat: todo 작성 api 연결

--- front/src/components/todo/Todo.tsx
+++ front/src/components/todo/Todo.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { getAllTodos } from '../../api/requests';
+import { getAllTodos, createTodo } from '../../api/requests';
 import './Todo.scss';
 
 function Todo(props: any) {
   const [todos, setTodos] = useState('');
+  const [content, setContnet] = useState('');
+  const [checkDone, setCheckDone] = useState(false);
 
   useEffect(() => {
     try {
@@ -15,10 +17,39 @@ function Todo(props: any) {
     }
   }, []);
 
+  const handleContent = (e: any) => {
+    setContnet(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    try {
+      createTodo(content, checkDone).then((data) => {
+        console.log(data, 'handleSubmit 완료');
+      });
+    } catch (e) {
+      console.log(e, 'handleSubmit 에러');
+    }
+  };
+
+  const handleCheckbox = () => {
+    setCheckDone(!checkDone);
+  };
+
   return (
     <div>
-      <input placeholder="todo를 입력하세요" />
-      <button>작성완료</button>
+      <input
+        placeholder="todo를 입력하세요"
+        onChange={(e) => handleContent(e)}
+        value={content}
+      />
+      <input
+        type="checkbox"
+        className="done__Checkbox"
+        onChange={handleCheckbox}
+      />
+      <label htmlFor="done__Checkbox">완료</label>
+      <br />
+      <button onClick={handleSubmit}>작성완료</button>
       <h3>4/13 todoHeader</h3>
       <div>{todos}</div>
       <p>todoContent</p>
