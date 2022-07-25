9c1de5b2eea84b6be1f4cd03e364eec220939a1f
maltepoo <jaengpin@gmail.com>
2022-07-25 23:06:48 +0900

    feat: api header authorization 추가

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -3,11 +3,13 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:8000/';
 
 const createAxiosInstance = () => {
+  const jwtToken = localStorage.getItem('jwt');
   const axiosInstance = axios.create({
     baseURL: BASE_URL,
     headers: {
       'Content-type': 'application/json',
       // 'Access-Control-Allow-Credentials': true,
+      'Authorization': jwtToken,
     },
   });
 
