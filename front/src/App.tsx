338201253e63c186026a0031ba7d0ddcb98739a9
maltepoo <jaengpin@gmail.com>
2022-08-02 00:16:19 +0900

    feat: todoDrawer 전역상태변수로 변경

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,4 @@
+import { createContext, useReducer, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.module.scss';
@@ -7,16 +8,41 @@ import Calander from './pages/Calander';
 import Login from './pages/Login';
 import PersonalSettings from './pages/PersonalSettings';
 
+type initialValueType = {
+  todoState: boolean;
+  toggleTodoState: any;
+};
+
+const initialValue: initialValueType = {
+  todoState: false,
+  toggleTodoState: () => null,
+};
+
+export const ContextApi = createContext(initialValue);
+
 function App() {
+  const [todoState, settodoState] = useState(false);
+  const toggleTodoState = () => {
+    settodoState((prev) => {
+      return !prev;
+    });
+  };
+  const contextValue = {
+    todoState: todoState,
+    toggleTodoState: toggleTodoState,
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Intro />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/personalSettings" element={<PersonalSettings />} />
-        <Route path="/calander" element={<Calander />} />
-      </Routes>
-    </BrowserRouter>
+    <ContextApi.Provider value={contextValue}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/personalSettings" element={<PersonalSettings />} />
+          <Route path="/calander" element={<Calander />} />
+        </Routes>
+      </BrowserRouter>
+    </ContextApi.Provider>
   );
 }
 
